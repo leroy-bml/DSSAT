91ffbcb6cfb5a94f4f268f805bde35b00f735329
leroy-bml <benjamin.leroy13@gmail.com>
2025-10-23 15:28:52 +0200

    correct DATE format

--- R/wth_v_fmt.R
+++ R/wth_v_fmt.R
@@ -1,6 +1,7 @@
 wth_v_fmt <- function(section, old_format = FALSE){
   if(section == "DAILY"){
-      v_fmt <- c(DATE = "%7s", SRAD = "%6.1f", TMAX = "%6.1f",
+      v_fmt <- c(DATE = "%5s", # instead of %7s
+                 SRAD = "%6.1f", TMAX = "%6.1f",
                  TMIN = "%6.1f", RAIN = "%6.1f", WIND = "%6.0f",
                  RHUM = "%6.1f", DEWP = "%6.1f", PAR = "%6.1f",
                  EVAP = "%6.1f", VAPR = "%6.2f", SUNH = "%6.1f")
